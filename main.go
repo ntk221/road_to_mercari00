d2edafd89c91b50a8496793e604b5ee67eead9e8
ntk221 <nittakdub@gmail.com>
2023-04-23 20:47:03 +0900

    mainでlistFilesを呼び出す形でテスト

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"path/filepath"
-	"os"
 	"fmt"
+	"os"
+	"path/filepath"
 )
-
 
 // func ReadDir(name string) ([]DirEntry, error)
 // func Walk(root string, fn WalkFunc) error
@@ -13,7 +12,7 @@ import (
 // あるディレクトリの配下のファイル一覧を取得する
 func listFiles(dirPath string) ([]string, error) {
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		return nil, fmt.Errorf("directory '%s' does not exist", dirPath)
+		return nil, fmt.Errorf("error: %s: no such file or directory", dirPath)
 	}
 	files := []string{}
 	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
@@ -23,4 +22,26 @@ func listFiles(dirPath string) ([]string, error) {
 		return nil
 	})
 	return files, err
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Fprintln(os.Stderr, "invalid argument")
+		os.Exit(1)
+	}
+	files := []string{}
+	for _, dir := range args {
+		dirFiles, err := listFiles(dir)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			continue
+		}
+		files = append(files, dirFiles...)
+	}
+
+	// ファイルのパスを出力
+	for _, file := range files {
+		fmt.Println(file)
+	}
 }
